14670e0b19347e3fddf3f0686f13a84772dbc920
Miguel Guilherme <mguilherme82@gmail.com>
2020-12-26 21:50:14 +0000

    add an error when TOTP is not provided

--- otp.ts
+++ otp.ts
@@ -1,7 +1,8 @@
 import Behin from 'https://deno.land/x/behin/mod.ts';
 import 'https://deno.land/x/dotenv/load.ts';
 
-const secret = Deno.env.get('TOTP') ?? '';
+const secret = Deno.env.get('TOTP');
+if (!secret) throw new Error('TOTP is not provided');
 
 const token = Behin.totp.generate(secret);
 console.log(token);
