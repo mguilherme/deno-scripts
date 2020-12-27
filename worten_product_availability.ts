8781f6e3d1c6fb2d0e46abb48892d7e21bae2e11
Miguel Guilherme <mguilherme82@gmail.com>
2020-12-27 15:30:58 +0000

    add description

--- worten_product_availability.ts
+++ worten_product_availability.ts
@@ -1,24 +1,29 @@
 import {DOMParser} from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
 import {brightBlue, brightGreen, brightRed, brightYellow} from 'https://deno.land/std/fmt/colors.ts';
 
+const abbreviate = (text: string, num: number) => (text.length <= num) ? text : `${text.slice(0, num)}...`
+
 const productInfo = async (url: string) => {
     const res = await fetch(url);
     const html = await res.text();
 
     const doc: any = new DOMParser().parseFromString(html, 'text/html');
 
-    const description = doc.querySelector('.pdp-product__title h1').textContent;
+    const title = doc.querySelector('.pdp-product__title h1').textContent;
     const available = !doc.querySelectorAll('.w-product__actions')[1].querySelector('div').className.endsWith('unavailable')
     const currentPrice = doc.querySelector('.w-product__price__current').getAttribute('content')
     const currency = doc.querySelector('.w-product-price__currency').textContent
+    const description = doc.querySelector('.w-product-about__info__wrapper').innerHTML
 
-    return {description, currency, currentPrice, available}
+
+    return {title, currency, currentPrice, available, description}
 };
 
 const printContent = (content: any) => {
-    console.log(`${brightYellow("Description =>")} ${brightBlue(content.description)}`);
+    console.log(`${brightYellow("Title =>")} ${brightBlue(content.title)}`);
     console.log(`${brightYellow("Price =>")} ${brightBlue(content.currentPrice)}`);
-    console.log(`${brightYellow("Availability =>")} ${content.available ? brightGreen('Available!') : brightRed('Unavailable!')}\n`);
+    console.log(`${brightYellow("Availability =>")} ${content.available ? brightGreen('Available!') : brightRed('Unavailable!')}`);
+    console.log(`${brightYellow("Description =>")} ${brightBlue(abbreviate(content.description, 50))}\n`)
 };
 
 const products = [
