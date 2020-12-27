27d534663082bcce4e1bd20d57e2073257f3fbd7
Miguel Guilherme <mguilherme82@gmail.com>
2020-12-27 16:41:40 +0000

    add table

--- worten_product_availability.ts
+++ worten_product_availability.ts
@@ -1,38 +1,42 @@
 import {DOMParser} from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
-import {brightBlue, brightGreen, brightRed, brightYellow} from 'https://deno.land/std/fmt/colors.ts';
+import {brightBlue, brightGreen, brightMagenta, brightRed, brightYellow} from 'https://deno.land/std/fmt/colors.ts';
+import {Table} from "https://deno.land/x/cliffy@v0.16.0/table/mod.ts";
 
-const abbreviate = (text: string, num: number) => (text.length <= num) ? text : `${text.slice(0, num)}...`
-
-const productInfo = async (url: string) => {
+const productDetail = async (url: string) => {
     const res = await fetch(url);
     const html = await res.text();
 
     const doc: any = new DOMParser().parseFromString(html, 'text/html');
 
     const title = doc.querySelector('.pdp-product__title h1').textContent;
-    const available = !doc.querySelectorAll('.w-product__actions')[1].querySelector('div').className.endsWith('unavailable')
-    const currentPrice = doc.querySelector('.w-product__price__current').getAttribute('content')
-    const currency = doc.querySelector('.w-product-price__currency').textContent
-    const description = doc.querySelector('.w-product-about__info__wrapper').innerHTML
+    const available = !doc.querySelectorAll('.w-product__actions')[1].querySelector('div').className.endsWith('unavailable');
+    const currentPrice = doc.querySelector('.w-product__price__current').getAttribute('content');
+    const currency = doc.querySelector('.w-product-price__currency').textContent;
+    const description = doc.querySelector('.w-product-about__info__wrapper').innerHTML;
 
-
-    return {title, currency, currentPrice, available, description}
+    return {title, currency, currentPrice, available, description, url};
 };
 
-const printContent = (content: any) => {
-    console.log(`${brightYellow("Title =>")} ${brightBlue(content.title)}`);
-    console.log(`${brightYellow("Price =>")} ${brightBlue(content.currentPrice)}`);
-    console.log(`${brightYellow("Availability =>")} ${content.available ? brightGreen('Available!') : brightRed('Unavailable!')}`);
-    console.log(`${brightYellow("Description =>")} ${brightBlue(abbreviate(content.description, 50))}\n`)
-};
+const printTable = (products: any[]) => {
+    new Table()
+        .header([brightYellow("Title"), brightYellow("Price"), brightYellow("Availability"), brightYellow("Link")])
+        .body(
+            products.map(p => {
+                const available = p.available ? brightGreen('Available!') : brightRed('Unavailable!');
+                return [brightBlue(p.title), brightMagenta(p.currentPrice), available, "-"]
+            })
+        )
+        .padding(1)
+        .indent(2)
+        .border(true)
+        .render();
+}
 
-const products = [
+const links = [
     'https://www.worten.pt/gaming/playstation/consolas/ps5/consola-ps5-825gb-7196053',
     'https://www.worten.pt/gaming/playstation/consolas/ps5/consola-ps5-edicao-digital-825-gb-7196054',
     'https://www.worten.pt/gaming/xbox/consolas/xbox-series-x-s/consola-xbox-series-x-1-tb-7240976'
 ];
 
-products.forEach(async url => {
-    const info = await productInfo(url);
-    printContent(info)
-})
+const products = await Promise.all(links.map(async url => await productDetail(url)));
+printTable(products);
